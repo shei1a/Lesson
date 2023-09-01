7664ce30b38f4a44616dfc93d27cc8134da03f9f
shei1a <143515016+shei1a@users.noreply.github.com>
2023-09-01 13:41:08 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,18 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+def main():
+    try:
+        num = int(input("Enter a number: "))
+        if num < 0:
+            print("Factorial is not defined for negative numbers.")
+        else:
+            result = factorial(num)
+            print(f"The factorial of {num} is: {result}")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
